2caf612094fc14de80286ea09ed1265da0b36130
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-08-31 01:05:17 -0300

    Ajustando consulta 3

--- monetdb/data/consultas.sql
+++ monetdb/data/consultas.sql
@@ -1,10 +1,12 @@
+
 -- Cada execução tem um id em dataflow e seu timestamp em dataflow_execution: consulta de tempos de execução para cada execução realizada.
 -- todas as execucoes foram executadas em sequencia ou houve paralelismo na execucao ? pq oq estou fazendo pra definir tempo de execucao eh pegar a hr de inicio da execucao seguinte e reduzir da hr de inicio da execucao atual
 select
 	t.id,
 	t.next_execution_datetime,
 	t.execution_datetime,
-	COALESCE(t.next_execution_datetime,t.execution_datetime) - t.execution_datetime as execution_time
+	coalesce(t.next_execution_datetime,
+	t.execution_datetime) - t.execution_datetime as execution_time
 from
 	(
 	select
@@ -22,7 +24,8 @@ from
 		de.execution_datetime,
 		d.id) t
 order by
-	execution_time,t.id;
+	execution_time,
+	t.id;
 -- Cada data transformation tem um id de execução em data_transformation_execution e o momento que começou a executar (timestamp): consulta de duração de execução por transformação.
 select
 	t.data_transformation_id,
@@ -52,6 +55,7 @@ group by
 -- 	A telemetria foi capturada em intervalos de 20/30s (uso de cpu, disco, memória). Em  telemetry tem o id de execução de cada transformação executada (data_transformation_execution_id): Consulta para identificar consumo de recurso por transformação (relacionando telemetry data_transformation_execution_id com data_transformation) e por dataflow.
 select
 	t.data_transformation_execution_id,
+	dt.tag,
 	count(t.data_transformation_execution_id) as count_data_transformation_exec,
 	avg(tm.svmem_total) as avg_svmem_total,
 	sum(tm.svmem_total) as sum_svmem_total,
@@ -78,12 +82,22 @@ select
 	--avg(cast( td.sswap_used as double)) as avg_sswap_used, => nao usado pq os valores estao com a string 'null'
 	--sum(cast( td.sswap_used as double)) as sum_sswap_used => nao usado pq os valores estao com a string 'null'
 from
-	telemetry t
+	data_transformation dt
+left join telemetry t on
+	(t.data_transformation_execution_id = dt.id)
+left join dataflow d on
+	(dt.df_id = d.id)
 left join telemetry_cpu tc on
 	(t.id = tc.telemetry_id)
 left join telemetry_disk td on
 	(t.id = td.telemetry_id )
 left join telemetry_memory tm on
 	(t.id = tm.telemetry_id)
+where
+	t.data_transformation_execution_id is not null
 group by
-	t.data_transformation_execution_id;
+	t.data_transformation_execution_id,
+	dt.tag
+order by
+	2 desc;
+--Consulta de domínio: quantitativo de modelos evolutivos calculados -> disponíveis nos datasets de  saída da transformação modelgeneratormodule_raxml ou _mrb (ds_omodelgeneratormodule_mrb, _raxml)
