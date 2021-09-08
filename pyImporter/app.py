5c371bf7cbd983f671500ca2186989e0f3c88943
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-09-07 23:36:41 -0300

    Iniciando comandos pra percorrer tables no monet e transpor informacoes pra outras bases de dados

--- pyImporter/app.py
+++ pyImporter/app.py
@@ -1,1 +1,37 @@
-print("Olá")
+import pymonetdb
+
+qtdPerPage = 10
+
+# import the SQL module
+# set up a connection. arguments below are the defaults
+monetdbConn = pymonetdb.connect(
+    username="monetdb", password="monetdb", hostname="localhost", database="sciphy_dados")
+
+# create a cursor
+cursor = monetdbConn.cursor()
+
+# increase the rows fetched to increase performance (optional)
+cursor.arraysize = 100
+
+limitString = ' LIMIT ' + str(qtdPerPage) + ' '
+
+dataflowConsulta = 'SELECT id,tag,user_id FROM dataflow'
+dataflowPage = 1
+
+cursor.execute(
+    'SELECT ceil(count(*)/' + str(qtdPerPage) + ') FROM (' + dataflowConsulta + ') x')
+dataflowUltimaPagina = int(cursor.fetchone()[0])
+
+while(dataflowPage <= dataflowUltimaPagina):
+    dataflowOffset = ''
+
+    if(dataflowPage > 1):
+        dataflowOffset = ' OFFSET ' + str((dataflowPage-1) * qtdPerPage) + ' '
+    else:
+        # Show the table meta data
+        dataflowMetadata = cursor.description
+
+    cursor.execute(dataflowConsulta+limitString+dataflowOffset)
+    dataflow = cursor.fetchall()
+
+    dataflowPage += 1
--- pyImporter/readme.md
+++ pyImporter/readme.md
@@ -1,7 +1,10 @@
-# Comandos
+# pyImporter
+* Aplicaçãozinha em python pra copiar dados que estão no monetDb para os outros bancos de dados
+## Comandos
 * python3 -m pip install --user --upgrade pip
     * python3 -m pip --version
 * python3 -m pip install --user virtualenv
+* python3 -m venv ./env
 * source env/bin/activate
     * Activating a virtual environment
     * Leaving the virtual environment
@@ -10,8 +13,10 @@
     * pip install -r requirements.txt
 * cria requirements
     * pip freeze > requirements.txt
-# Paradigmas Python
+## Paradigmas Python
 * procedural
 * orientação a objetos
 * funcional
 * imperativa
+## Coisas relevantes
+* Criar um arquivo `__init__.py` em uma pasta vai fazer o python tratar isso como um modulo
