31ad6615d4e1ad59f304fc12ff892e64b1ba9c26
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-09-25 01:21:44 -0300

    Preparando insert de collections

--- pyImporter/mongoImporter.py
+++ pyImporter/mongoImporter.py
@@ -1,7 +1,10 @@
+from pymongo import results
 from data.mongoRepository import MongoRepository
 from data.monetRepository import MonetRepository
 
 rodando_no_docker = False
+qtdPerPage = 5
+verbose_level = 2
 
 host_mongo = "localhost"
 host_monetdb = "localhost"
@@ -16,9 +19,63 @@ monetdbConn = MonetRepository(
     host_monetdb, "sciphy_dados", "monetdb", "monetdb")
 
 
-importArr = {}
+importDictionary = {}
 
 dataflow = {}
 dataflow["consulta"] = "select   d.id,   d.tag,   sys.str_to_timestamp(de.execution_datetime, '%Y-%m-%d %H:%M:%S') as execution_datetime  from   dataflow d  left join dataflow_execution de on   (d.id = de.df_id)  order by   de.execution_datetime,   d.id"
 
-importArr["dataflow"] = dataflow
+importDictionary["dataflow"] = dataflow
+
+
+def insere_documentos(dictionary: dict, chave_pai: str = ""):
+    chaves = dictionary.keys()
+    for chave in chaves:
+        if(chave == "consulta"):
+            consulta = dictionary[chave]
+
+            limit = ' LIMIT ' + str(qtdPerPage) + ' '
+
+            pagina = 1
+
+            ultima_pagina = int(monetdbConn.consultar(
+                'SELECT ceil(count(*)/' + str(qtdPerPage) + ') FROM (' + consulta + ') x')[0][0])
+
+            if ultima_pagina < 1:
+                ultima_pagina = 1
+
+            while(pagina <= ultima_pagina):
+                if verbose_level > 0:
+                    print('percorrendo a pagina ' + str(pagina) + ' de ' +
+                          str(ultima_pagina) + ' da consulta ' + consulta + '\n')
+                offset = ''
+
+                if(pagina > 1):
+                    offset = ' OFFSET ' + str((pagina-1) * qtdPerPage) + ' '
+
+                mongoConn.insere(
+                    chave_pai, tuple_list_to_dictionary(consulta+limit+offset))
+
+                pagina += 1
+        else:
+            insere_documentos(dictionary[chave], chave)
+
+
+def tuple_list_to_dictionary(consulta: str):
+    estrutura = monetdbConn.recupera_estrutura(consulta)
+    resultado = monetdbConn.consultar(consulta)
+
+    retorno = []
+
+    for tupla in resultado:
+        dicionario = {}
+        estutura_campo_indice = 0
+        for campo in tupla:
+            dicionario[estrutura[estutura_campo_indice].name] = campo
+            estutura_campo_indice += 1
+
+        retorno.append(dicionario)
+
+    return retorno
+
+
+insere_documentos(importDictionary)
