3e2c6558b649528c56aa0f1c4c6809cbf72180dc
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-10-26 02:24:07 -0300

    Dando forma ao pyImporter, permitindo inserir subdocumentos no mongo

--- pyImporter/mongoImporter.py
+++ pyImporter/mongoImporter.py
@@ -1,5 +1,6 @@
 from data.mongoRepository import MongoRepository
 from data.monetRepository import MonetRepository
+import re
 
 rodando_no_docker = False
 qtdPerPage = 5
@@ -19,62 +20,74 @@ monetdbConn = MonetRepository(
 
 
 importDictionary = {}
-
 dataflow = {}
-dataflow["consulta"] = "select   d.id,   d.tag,   sys.str_to_timestamp(de.execution_datetime, '%Y-%m-%d %H:%M:%S') as execution_datetime  from   dataflow d  left join dataflow_execution de on   (d.id = de.df_id)  order by   de.execution_datetime,   d.id"
-
+dataflow["consulta"] = "select d.id, d.tag from dataflow d order by d.id"
+dataflow_dataflon = {}
+dataflow_dataflon["consulta"] = "select sys.str_to_timestamp(de.execution_datetime, '%Y-%m-%d %H:%M:%S') as execution_datetime from dataflow_execution de where de.df_id = #$id$#  order by   de.execution_datetime"
+dataflow["execution"] = dataflow_dataflon
 data_transformation = {}
-data_transformation[
-    "consulta"] = "select   dte.id,   dte.dataflow_execution_id,   dte.data_transformation_id,   dt.tag as data_transformation_tag,   sys.str_to_timestamp(dte.execution_datetime, '%Y-%m-%d %H:%M:%S') as execution_datetime  from   data_transformation_execution dte  left join data_transformation dt on   (dt.id = dte.data_transformation_id)"
-
+data_transformation["consulta"] = "select dt.id, dt.tag as data_transformation_tag from data_transformation dt"
+data_transformation["execution"] = "select dte.id, dte.dataflow_execution_id, dte.data_transformation_id, TO_TIMESTAMP(dte.execution_datetime, 'YYYY-MM-DD HH24:MI:SS') as current_execution_datetime, LEAD( TO_TIMESTAMP(dte.execution_datetime, 'YYYY-MM-DD HH24:MI:SS') ) over ( order by dte.execution_datetime, dte.id) as next_execution_datetime from data_transformation_execution dte where dte.id = #$id$#"
 importDictionary["dataflow"] = dataflow
 importDictionary["data_transformation"] = data_transformation
 
 
-def insere_documentos(dictionary: dict, chave_pai: str = ""):
+def insere_documentos(dictionary: dict, nivel: int = 0):
     chaves = dictionary.keys()
+
+    if 'consulta' in dictionary:
+        result = get_dict_by_consulta(dictionary['consulta'])
+
     for chave in chaves:
-        if(chave == "consulta"):
-            consulta = dictionary[chave]
+        if(nivel == 0):
+            mongoConn.insere(chave, insere_documentos(
+                dictionary[chave], nivel+1))
 
-            limit = ' LIMIT ' + str(qtdPerPage) + ' '
+        if chave != 'consulta':
+            if(type(dictionary[chave]) == str):
 
-            pagina = 1
+                consulta = str(dictionary[chave])
+                variaveis = re.findall(r'#$.*$#', consulta)
 
-            ultima_pagina = int(monetdbConn.consultar(
-                'SELECT ceil(count(*)/' + str(qtdPerPage) + ') FROM (' + consulta + ') x')[0][0])
+                for variavel in variaveis:
+                    consulta = consulta.replace(
+                        '#$'+variavel+'$#', result[variavel])
 
-            if ultima_pagina < 1:
-                ultima_pagina = 1
+                result[chave] = get_dict_by_consulta(dictionary[chave])
+            else:
+                result[chave] = insere_documentos(dictionary[chave], nivel+1)
 
-            while(pagina <= ultima_pagina):
-                if verbose_level > 0:
-                    print('percorrendo a pagina ' + str(pagina) + ' de ' +
-                          str(ultima_pagina) + ' da consulta ' + consulta + '\n')
-                offset = ''
-
-                if(pagina > 1):
-                    offset = ' OFFSET ' + str((pagina-1) * qtdPerPage) + ' '
-
-                dict_to_insert = tuple_list_to_dictionary(
-                    consulta+limit+offset)
-
-                mongoConn.insere(
-                    chave_pai, dict_to_insert)
-
-                pagina += 1
-        else:
-            insere_documentos(dictionary[chave], chave)
+    return result
 
 
-def recupera_retornos_pra_colocar_dicionario_a_ser_inserido(dictionary: dict):
-    # apenas teste por enquanto
-    chaves = dictionary.keys()
-    retorno = {}
-    for chave in chaves:
-        if(chave != "consulta"):
-            dict_obj = dictionary[chave]
-            monetdbConn.consultar(dict_obj['obj'])
+def get_dict_by_consulta(consulta: str):
+    limit = ' LIMIT ' + str(qtdPerPage) + ' '
+
+    retorno = []
+
+    pagina = 1
+
+    ultima_pagina = int(monetdbConn.consultar(
+        'SELECT ceil(count(*)/' + str(qtdPerPage) + ') FROM (' + consulta + ') x')[0][0])
+
+    if ultima_pagina < 1:
+        ultima_pagina = 1
+
+    while(pagina <= ultima_pagina):
+        if verbose_level > 0:
+            print('percorrendo a pagina ' + str(pagina) + ' de ' +
+                  str(ultima_pagina) + ' da consulta ' + consulta + '\n')
+        offset = ''
+
+        if(pagina > 1):
+            offset = ' OFFSET ' + str((pagina-1) * qtdPerPage) + ' '
+
+        dict_to_insert = tuple_list_to_dictionary(
+            consulta+limit+offset)
+
+        pagina += 1
+
+        retorno.append(dict_to_insert)
 
     return retorno
 
