8e0358766b2dae0bd81c9f212b2e80304d849f78
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-12-07 23:29:31 -0300

    Finalizando a 2a consulta pro neo4j

--- pyImporter/queryComparator.py
+++ pyImporter/queryComparator.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from data.neo4jRepository import Neo4jRepository
 from data.postgresRepository import PostgresRepository
 from data.monetRepository import MonetRepository
 from data.mongoRepository import MongoRepository
@@ -8,10 +9,16 @@ rodando_no_docker = True
 host_mongo = "localhost"
 host_postgres = "localhost"
 host_monetdb = "localhost"
+host_neo4j = "neo4j:bolt://localhost"
 if rodando_no_docker:
     host_mongo = "projeto_aplicacao_mongodb"
     host_postgres = "projeto_aplicacao_postgres"
     host_monetdb = "projeto_aplicacao_monetdb"
+    host_neo4j = "bolt://projeto_aplicacao_neo4j"
+
+
+neo4jConn = Neo4jRepository(uri=host_neo4j + ":7687/",
+                            user="neo4j", password="senha_neo4j")
 
 pgsqlConn = PostgresRepository(
     host_postgres, "public", "postgres", "postgres")
@@ -35,6 +42,13 @@ consultas_monet = [
     "select  t.data_transformation_id,  t.data_transformation_tag,  sum(coalesce(t.next_execution_datetime,  t.current_execution_datetime) - t.current_execution_datetime) as execution_time from  (  select   dte.id,   dte.dataflow_execution_id,   dte.data_transformation_id,   dt.tag as data_transformation_tag,   sys.str_to_timestamp(dte.execution_datetime, '%Y-%m-%d %H:%M:%S') as current_execution_datetime,   LEAD( sys.str_to_timestamp(dte.execution_datetime, '%Y-%m-%d %H:%M:%S') ) over (  order by   dte.execution_datetime,   dte.id) as next_execution_datetime  from   data_transformation_execution dte  left join data_transformation dt on   (dt.id = dte.data_transformation_id)   ) t group by  t.data_transformation_id,  t.data_transformation_tag",
     "select  t.data_transformation_execution_id,  dt.tag,  count(t.data_transformation_execution_id) as count_data_transformation_exec,  avg(tm.svmem_total) as avg_svmem_total,  sum(tm.svmem_total) as sum_svmem_total,  avg(tm.svmem_available) as avg_svmem_available,  sum(tm.svmem_available) as sum_svmem_available,  avg(tm.svmem_used) as avg_svmem_used,  sum(tm.svmem_used) as sum_svmem_used,  avg(cast( tc.scputimes_user as double)) as avg_scputimes_user,  sum(cast( tc.scputimes_user as double)) as sum_scputimes_user,  avg(cast( tc.scputimes_system as double)) as avg_scputimes_system,  sum(cast( tc.scputimes_system as double)) as sum_scputimes_system,  avg(cast( tc.scputimes_idle as double)) as avg_scputimes_idle,  sum(cast( tc.scputimes_idle as double)) as sum_scputimes_idle,  avg(cast( tc.scputimes_steal as double)) as avg_scputimes_steal,  sum(cast( tc.scputimes_steal as double)) as sum_scputimes_steal,  avg(cast( td.sdiskio_read_bytes as double)) as avg_sdiskio_read_bytes,  sum(cast( td.sdiskio_read_bytes as double)) as sum_sdiskio_read_bytes,  avg(cast( td.sdiskio_write_bytes as double)) as avg_sdiskio_write_bytes,  sum(cast( td.sdiskio_write_bytes as double)) as sum_sdiskio_write_bytes,  avg(cast( td.sdiskio_busy_time as double)) as avg_sdiskio_busy_time,  sum(cast( td.sdiskio_busy_time as double)) as sum_sdiskio_busy_time,  avg(cast( td.sswap_total as double)) as avg_sswap_total,  sum(cast( td.sswap_total as double)) as sum_sswap_total from  data_transformation dt left join telemetry t on  (t.data_transformation_execution_id = dt.id) left join dataflow d on  (dt.df_id = d.id) left join telemetry_cpu tc on  (t.id = tc.telemetry_id) left join telemetry_disk td on  (t.id = td.telemetry_id ) left join telemetry_memory tm on  (t.id = tm.telemetry_id) where  t.data_transformation_execution_id is not null group by  t.data_transformation_execution_id,  dt.tag order by  2 desc",
     "select  uniao.qtd,  uniao.model,  uniao.program from  (  select   mrb.*  from   (   select    count(*) qtd,    domrb.model,    'mrb' program   from    ds_omodelgeneratormodule_mrb domrb   group by    domrb.model) mrb union  select   raxml.*  from   (   select    count(*) qtd,    doraxml.model,    'raxml' program   from    ds_omodelgeneratormodule_raxml doraxml   group by    doraxml.model) raxml) uniao order by 1 desc"
+]
+
+consultas_neo4j = [
+    "MATCH (d:Dataflow)-->(e:DataflowExecution) RETURN d.id AS id,e.execution_datetime_start AS execution_dattime,e.execution_datetime_end AS next_execution_datetime, duration.inSeconds(e.execution_datetime_start,e.execution_datetime_end).seconds AS execution_time ORDER BY execution_time, id",
+    "MATCH (d:DataTransformation)-->(e:DataTransformationExecution) RETURN d.id AS id,d.tag AS tag,toInteger(sum(duration.inSeconds(e.execution_datetime_start,e.execution_datetime_end).seconds)) AS execution_time ORDER BY d.id",
+    "",
+    ""
 ]
 
 mongo_pipes = [
@@ -176,7 +190,8 @@ for i in range(0, 4):
         consultas_postgres[i]).microseconds
     novo_item["monet"] = monetdbConn.recupera_tempo(
         consultas_monet[i]).microseconds
-    novo_item["neo4j"] = None
+    novo_item["neo4j"] = neo4jConn.recupera_tempo(
+        consultas_neo4j[i], {}).microseconds
     novo_item["created_at"] = datetime.now()
 
     mongoConn.insere("reports", [novo_item])
